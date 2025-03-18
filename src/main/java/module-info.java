e7c05cb6730ed6a920368cd85b4467d0b0e824a8
TheTrueJM <joshuamalinao05@gmail.com>
2025-03-18 11:55:26 +1000

    JavaFX Repo Setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ai.tutor.cab302exceptionalhandlers {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ai.tutor.cab302exceptionalhandlers to javafx.fxml;
+    exports ai.tutor.cab302exceptionalhandlers;
+}
