6de2038a6ed29a62139ab1f1eed688a3c0e389c2
TheTrueJM <161416439+TheTrueJM@users.noreply.github.com>
2025-04-14 15:28:38 +1000

    added basic Database Access Objects

---
+++ src/main/java/ai/tutor/cab302exceptionalhandlers/model/SQLiteConnection.java
@@ -0,0 +1,25 @@
+package ai.tutor.cab302exceptionalhandlers.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+    private static Connection instance = null;
+
+    public SQLiteConnection() {
+        String url = "jdbc:sqlite:tutor.db";
+        try {
+            instance = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage() + " - " + e.toString());
+        }
+    }
+
+    public static  Connection getInstance() {
+        if (instance == null) {
+            new SQLiteConnection();
+        }
+        return instance;
+    }
+}
