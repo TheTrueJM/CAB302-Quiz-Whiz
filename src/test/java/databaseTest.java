23165bf430c085ba8a327af9d6ba1c4d7799ccb7
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2025-04-16 04:35:32 +1000

    Added back spaces lol

--- src/test/java/databaseTest.java
+++ src/test/java/databaseTest.java
@@ -7,15 +7,15 @@ import java.sql.Connection;
 import ai.tutor.cab302exceptionalhandlers.model.SQLiteConnection;
 
 public class databaseTest {
-	private Connection db = null;
+    private Connection db = null;
 
-	@BeforeEach
-	public void setUp() {
-		db = SQLiteConnection.getInstance();
-	}
+    @BeforeEach
+    public void setUp() {
+        db = SQLiteConnection.getInstance();
+    }
 
-	@Test
-	public void testDatabaseConnection() {
-		assertEquals(true, db != null);
-	}
+    @Test
+    public void testDatabaseConnection() {
+        assertEquals(true, db != null);
+    }
 }
