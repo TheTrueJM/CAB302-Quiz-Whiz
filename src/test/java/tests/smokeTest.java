286a624da8bde8877a4c7c918ea03c01ce64e1a1
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2025-04-24 07:44:41 +1000

    Use wildcard imports for unit tests

--- src/test/java/tests/smokeTest.java
+++ src/test/java/tests/smokeTest.java
@@ -1,7 +1,7 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 public class smokeTest {
     @Test
