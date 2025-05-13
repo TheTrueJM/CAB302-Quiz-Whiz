7b03b0d79c6f099a3a089eb4d19c4802000d6344
Shoxys <167745094+Shoxys@users.noreply.github.com>
2025-05-13 16:18:46 +1000

    Implement return button functionality

--- src/main/java/ai/tutor/cab302exceptionalhandlers/controller/QuizController.java
+++ src/main/java/ai/tutor/cab302exceptionalhandlers/controller/QuizController.java
@@ -45,6 +45,7 @@ public class QuizController {
     private QuizQuestionDAO quizQuestionDAO;
     private AnswerOptionDAO answerOptionDAO;
     private UserAnswerDAO userAnswerDAO;
+    private User currentUser;
     //Extra added for quiz functionality
     private int questionNumber;
     private List<QuizQuestion> quizQuestions = new ArrayList<>();
